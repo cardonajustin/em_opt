b0950bf853eb570da19a3af4ae6c0168d4a6cfa2
rjustincardona <rjustincardona@gmail.com>
2025-01-17 13:45:30 -0500

    implemented gradient descent

--- src/opt_problem.jl
+++ src/opt_problem.jl
@@ -1,7 +1,7 @@
 module opt_problem
 include("utils.jl")
 using .utils, GilaElectromagnetics, CUDA, LinearAlgebra, Roots, JacobiDavidson, BaryRational
-export OptProblem, objective, constraint_re, constraint_im, lagrangian, get_LTT_E, partial_dual_constraint, partial_dual_root, pade_root
+export OptProblem, objective, constraint_re, constraint_im, lagrangian, get_LTT_E, partial_dual_constraint, partial_dual_root, partial_dual_fn, partial_dual_fn_dl, partial_dual, pade_root
 
 mutable struct OptProblem
 	G::GlaOpr
@@ -27,6 +27,7 @@ function objective(prob::OptProblem, T::AbstractArray{ComplexF64})
 	return real(imag(prob.S' * T) - T' * (prob.Q * T))
 end
 
+
 function constraint_re(prob::OptProblem, i::Int64, T::AbstractArray{ComplexF64})
     ASym = M::AbstractMatrix -> -0.5im * (M - adjoint(M))
 	P = CUDA.zeros(size(prob.G, 2))
@@ -34,6 +35,7 @@ function constraint_re(prob::OptProblem, i::Int64, T::AbstractArray{ComplexF64})
 	P = CUDA.Diagonal(P)
 	return real(imag(prob.S' * (P * T)) +  T' * (ASym(adjoint(inv(prob.X))* P) * T) + 0.5*im * (T' * (adjoint(prob.G)*T) - T' * (P * (prob.G*T))))
 end
+
 
 function constraint_im(prob::OptProblem, i::Int64, T::AbstractArray{ComplexF64})
     Sym = M::AbstractMatrix -> 0.5 * (M + adjoint(M))
@@ -43,46 +45,86 @@ function constraint_im(prob::OptProblem, i::Int64, T::AbstractArray{ComplexF64})
 	return real(real(prob.S' * (P * T)) +  T' * (Sym(adjoint(inv(prob.X)) * P) * T) - 0.5 * (T' * (adjoint(prob.G)*(P * T)) + T' * (P * (prob.G*T))))
 end
 
-
-function lagrangian(prob::OptProblem, T::AbstractArray{ComplexF64}, a::AbstractMatrix{ComplexF64}, b::AbstractMatrix{ComplexF64})
-	c_re = zeros(ComplexF64, size(a, 1))
-	c_im = zeros(ComplexF64, size(a, 1))
+function multiplier_matrix(prob::OptProblem, a::AbstractArray{Float64}, b::AbstractArray{Float64})
+	A = zeros(ComplexF64, size(prob.X, 1))
+	B = zeros(ComplexF64, size(prob.X, 1))
 	for i in 1:size(prob.d, 1)
-		c_re[prob.d[i]] .+= constraint_re(prob, i, T)
-		c_im[prob.d[i]] .+= constraint_im(prob, i, T)
+		A[prob.d[i]] .+= ComplexF64(a[i])
+		B[prob.d[i]] .+= ComplexF64(b[i])
 	end
+	return CUDA.Diagonal(CuArray(A)), CUDA.Diagonal(CuArray(B))
+end
 
-	return real(objective(prob, T) + sum(a * CuArray(c_re)) + sum(b * CuArray(c_im)))
+function lagrangian(prob::OptProblem, T::AbstractArray{ComplexF64}, a::AbstractArray{Float64}, b::AbstractArray{Float64})
+	c_re, c_im = partial_dual_fn_dl(prob, a, b, 0.0)
+	return real(objective(prob, T) + sum(a .* c_re) + sum(b .* c_im))
 end
 
 
-
-function get_LTT_E(prob::OptProblem, z::Float64, a::AbstractMatrix{ComplexF64}, b::AbstractMatrix{ComplexF64})
+function get_LTT_E(prob::OptProblem, z::Float64, A::AbstractArray{Float64}, B::AbstractArray{Float64})
+	a, b = multiplier_matrix(prob, A, B)
     Sym = M::AbstractMatrix -> 0.5 * (M + adjoint(M))
     ASym = M::AbstractMatrix -> -0.5im * (M - adjoint(M))
     l = a - im*(b + z*I)
     LTT = x-> prob.Q*x + Sym(adjoint(inv(prob.X))*l)*x - 0.5 * (adjoint(prob.G)*(l*x)) - 0.5 * (adjoint(l)*(prob.G*x))
     E = x-> ASym(adjoint(inv(prob.X)))*x + 0.5*im * (adjoint(prob.G)*x - prob.G*x)
-    return LTT, E
-end
-
-
-function partial_dual_constraint(prob::OptProblem, z::Float64, a::AbstractMatrix{ComplexF64}, b::AbstractMatrix{ComplexF64})
-    LTT, E = get_LTT_E(prob, z, a, b)
     LTS = 0.5 * (a + im * (b + z * I))
-    T, mvp = LTT \ (LTS * prob.S)
-    return real(imag(prob.S' * T) - T' * E(T)), mvp
+    return LTT, E, LTS
 end
 
 
-function partial_dual_root(prob::OptProblem, a::AbstractMatrix{ComplexF64}, b::AbstractMatrix{ComplexF64})
-    LTT, E = get_LTT_E(prob, 0.0, a, b)
+function partial_dual_constraint(prob::OptProblem, z::Float64, a::AbstractArray{Float64}, b::AbstractArray{Float64})
+    LTT, E, LTS = get_LTT_E(prob, z, a, b)
+    T, mvp = LTT \ (LTS * prob.S)
+    return real(imag(prob.S' * T) - T' * E(T)), mvp, T
+end
+
+
+function partial_dual_root(prob::OptProblem, a::AbstractArray{Float64}, b::AbstractArray{Float64})
+    LTT, E, _ = get_LTT_E(prob, 0.0, a, b)
 	g, _ = powm_gpu(LTT, E, size(prob.G, 2))
 	if real(g) > 0
 		g, _ = powm_gpu(LTT, E, size(prob.G, 2), g)
 	end
 	g = -real(g)
-	return pade_root(x-> partial_dual_constraint(prob, x, a, b)[1], g + sqrt(abs(g)))
+	return pade_root(x-> partial_dual_constraint(prob, x, a, b)[1], g + sqrt(abs(g)), max_iter=10)
+end
+
+
+function partial_dual_fn(prob::OptProblem, a::AbstractArray{Float64}, b::AbstractArray{Float64}, g::Float64)
+	C, _, T = partial_dual_constraint(prob, g, a, b)
+	return lagrangian(prob, T, a, b) + g * C
+end
+
+function partial_dual_fn_dl(prob::OptProblem, a::AbstractArray{Float64}, b::AbstractArray{Float64}, g::Float64)
+	_, _, T = partial_dual_constraint(prob, g, a, b)
+	d_re = map(i-> constraint_im(prob, i, T), 1:size(a, 1))
+	d_im = map(i-> constraint_im(prob, i, T), 1:size(b, 1))
+	#d_re = zeros(Float64, size(a, 1))
+	#d_im = zeros(Float64, size(b, 1))
+	#for i in 1:size(prob.d, 1)
+	#	d_re[i] = constraint_re(prob, i, T)
+	#	d_im[i] = constraint_im(prob, i, T)
+	#end
+	return d_re, d_im
+end
+
+
+function partial_dual(prob::OptProblem, a0::AbstractArray{Float64}, b0::AbstractArray{Float64}, g0::Float64, tol::Float64=1e-10, max_iter::Int=5, learning_rate::Float64=1e-0)
+	a = a0
+	b = b0
+	g = g0
+	for iter in 1:max_iter
+		g_temp = pade_root(z-> partial_dual_constraint(prob, z, a, b)[1], g)[1]
+		gradient = partial_dual_fn_dl(prob, a, b, g_temp)
+		err = norm(gradient)
+		err > tol / size(gradient, 1) || break
+		@show err
+		a -= (learning_rate) * gradient[1]
+		b -= (learning_rate) * gradient[2]
+		g = g_temp
+	end
+	return partial_dual_fn(prob, a, b, g)
 end
 
 
--- test/gen_data.jl
+++ test/gen_data.jl
@@ -2,18 +2,18 @@ include("../src/opt_problem.jl")
 using .opt_problem, CUDA, ProgressMeter, Serialization
 
 
-function zeros_perturbed(n_perturb::Int, n_volume::Int, dx::Float64=1e-2)
+function zeros_perturbed(n_perturb::Int, n_volume::Int, d::Vector{Vector{Int64}}, dx::Float64=1e-2)
     zs = zeros(ComplexF64, n_perturb)
     ns = zeros(Int, n_perturb)
-    C = OptProblem(n_volume)
-	a = CUDA.Diagonal(CUDA.randn(ComplexF64, size(C.G, 2)))
-	b = CUDA.Diagonal(CUDA.randn(ComplexF64, size(C.G, 2)))
+    C = OptProblem(n_volume, d)
+	a = rand(Float64, size(C.G, 2))
+	b = rand(Float64, size(C.G, 2))
 	g, _ = partial_dual_root(C, a, b)
 	println("initial guess done")
     for i in 1:n_perturb
 		println("\t" * string(i) * " perturbations")
-        a += CUDA.Diagonal(dx .* CUDA.randn(ComplexF64, size(C.G, 2)))
-        b += CUDA.Diagonal(dx .* CUDA.randn(ComplexF64, size(C.G, 2)))
+		a += dx .* randn(Float64, size(C.G, 2))
+		b += dx .* randn(Float64, size(C.G, 2))
 		result = pade_root(x-> partial_dual_constraint(C, x, a, b)[1], g)
         zs[i] = result[1]
         ns[i] = result[2]
@@ -23,26 +23,26 @@ function zeros_perturbed(n_perturb::Int, n_volume::Int, dx::Float64=1e-2)
 end
 
 
-function stat_test(n_samples::Int, n_perturb::Int, n_volume::Int, dx::Float64=1e-2)
+function stat_test(n_samples::Int, n_perturb::Int, n_volume::Int, d::Vector{Vector{Int64}}, dx::Float64=1e-2)
     zs = zeros(ComplexF64, n_samples, n_perturb)
     ns = zeros(Int, n_samples, n_perturb)
     for i in 1:n_samples
 		println("Sample " * string(i))
-        while true
-			result = zeros_perturbed(n_perturb, n_volume, dx)
-            zs[i, :] = result[1]
-            ns[i, :] = result[2]
-            try
-                result = zeros_perturbed(n_perturb, n_volume, dx)
-                zs[i, :] = result[1]
-                ns[i, :] = result[2]
-                break
-            catch e
-				@show e
-				println("Restarting at sample " * string(i))
-                continue
-            end
-        end
+		result = zeros_perturbed(n_perturb, n_volume, d, dx)
+		zs[i, :] = result[1]
+		ns[i, :] = result[2]
+        #while true
+        #    try
+        #        result = zeros_perturbed(n_perturb, n_volume, d, dx)
+        #        zs[i, :] = result[1]
+        #        ns[i, :] = result[2]
+        #        break
+        #    catch e
+		#		@show e
+		#		println("Restarting at sample " * string(i))
+        #        continue
+        #    end
+        #end
     end
     return zs, ns    
 end
@@ -55,13 +55,14 @@ dx=1e-3
 T = CUDA.rand(ComplexF64, 3 * n_volume^3) .- ComplexF64(0.5 + 0.5im)
 
 domains = [[1, 2, 3], [5, 7, 8], [1, 4, 9, 10], [4, 5]]
-
+a = ones(Float64, size(domains, 1))
+b = ones(Float64, size(domains, 1))
 p = OptProblem(n_volume, domains)
-a = ComplexF64.(CUDA.Diagonal(CUDA.rand(Float64, size(p.G, 2))))
-b = ComplexF64.(CUDA.Diagonal(CUDA.rand(Float64, size(p.G, 2))))
-@show lagrangian(p, T, a, b)
 
-#zs, ns = stat_test(n_samples, n_perturb, n_volume, dx)
+g = partial_dual_root(p, a, b)[1]
+s = partial_dual(p, a, b, g)
+@show s
+
+#zs, ns = stat_test(n_samples, n_perturb, n_volume, domains, dx)
 #serialize("data/zeros_"*string(n_volume)*"_"*string(dx)*".dat", zs)
 #serialize("data/iters_"*string(n_volume)*"_"*string(dx)*".dat", ns)
-
